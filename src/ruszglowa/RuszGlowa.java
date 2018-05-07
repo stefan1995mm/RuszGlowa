8db3df249bebcac0440f1682abcb88bc03ae6acf
stefan1995mm <dominik.stefanczyk@gmail.com>
2018-05-07 22:23:43 +0200

    TestNetbeans

--- src/ruszglowa/RuszGlowa.java
+++ src/ruszglowa/RuszGlowa.java
@@ -15,7 +15,7 @@ public class RuszGlowa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("testgit");
+        System.out.println("TEST GIT NETBEANS");
     }
     
 }
